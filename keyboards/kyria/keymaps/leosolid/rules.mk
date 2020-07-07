f33dd868b5b485cd35b5da97e95e73f1afc37774
Leo Solidum <leo.g.solidum@gmail.com>
2020-07-06 23:23:23 -0700

    save kyria layout

---
+++ keyboards/kyria/keymaps/leosolid/rules.mk
@@ -0,0 +1,4 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+EXTRAKEY_ENABLE = yes      # Enable Media and System Control
