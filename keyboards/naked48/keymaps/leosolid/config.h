0c1a0f277be17641c7f3ab751d1a5d6273a46921
Leo Solidum <leo.g.solidum@gmail.com>
2020-10-14 23:12:59 -0700

    add crkbd and naked48 layout

---
+++ keyboards/naked48/keymaps/leosolid/config.h
@@ -0,0 +1,37 @@
+/* Copyright 2018 Salicylic_acid3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Select hand configuration */
+
+//#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define USE_SERIAL
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define ONESHOT_TAP_TOGGLE 4
+
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
