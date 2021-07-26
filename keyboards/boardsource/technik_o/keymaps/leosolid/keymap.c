a8e71e2f8f8a9bebf9e0876a2ce473268421e7ad
Leo S <13348080+leosolid@users.noreply.github.com>
2021-07-25 20:51:54 -0700

    start technik o keymap

--- keyboards/boardsource/technik_o/keymaps/leosolid/keymap.c
+++ keyboards/boardsource/technik_o/keymaps/leosolid/keymap.c
@@ -1,52 +1,61 @@
-/* Copyright 2020 Boardsource
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// TODO RBG highlight OSM and LEADER key pending
+// TODO get MBK legend glows
+
 
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _MAIN,
-    _RAISE,
-    _LOWER,
+    _BASE,
+    _NAV,
+    _MOUSE,
+    _NUMBER,
+    _FUNCTION
 };
 
 // Readability keycodes
-#define LOWER   MO(_LOWER)
-#define RAISE   MO(_RAISE)
+#define XXXXXXX KC_NO
+#define _______ KC_TRNS
+
+// LT, Layer Toggles
+#define NAV_SPC   LT(_NAV,KC_SPACE)
+#define MOUSE_ESC LT(_MOUSE,KC_ESC)
+
+// OSL, One Shot Layers
+#define OSL_NUM   OSL(_NUMBER)
+
+// OSM, One Shot Modifier
+#define OSM_SHIFT OSM(SHIFT)
+#define OSM_HYPER OSM(MOD_HYPR)
+#define OSM_MEH   OSM(MOD_MEH)
+#define OSM_CAG   OSM(MOD_LCTL|MOD_LALT|MOD_GUI)
+#define OSM_SAG   OSM(MOD_LSFT|MOD_LALT|MOD_GUI)
+#define OSM_SUPER OSM(MOD_LCTL|MOD_LSFT|MOD_GUI) // CSG
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_ortho_4x12(
 
-  [_MAIN] = LAYOUT_ortho_4x12(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_GRAVE, KC_Q,         KC_W,        KC_F,         KC_P,         KC_B,    KC_J,    KC_L,        KC_U,         KC_Y,         KC_SCOLON,    KC_BSLS,
+    KC_BSPC,  LSFT_T(KC_A), LGUI_T(KC_R, LALT_T(KC_S), LCTL_T(KC_T), KC_G,    KC_M,    RCT_T(KC_N), RALT_T(KC_E), RGUI_T(KC_I), RSFT_T(KC_O), KC_QUOT,
+    KC_MINUS, KC_Z,         KC_X,        KC_C,         KC_D,         KC_V,    KC_K,    KC_H,        KC_COMM,      KC_DOT,       KC_SLSH,      KC_EQUAL,
+    KC_ESC,   _______,      _______,     KC_LEAD,      NAV,          _______, OSL_NUM, KC_ENT,      OSM_SHIFT,    _______,      _______,      ______
+    //MOUSE   ?             ?            ?             ?             ?        ?        ?            ?             ?             ?             ?
+
   ),
+  [_NAV] = LAYOUT_ortho_4x12(
 
-  [_RAISE] = LAYOUT_ortho_4x12(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-  RESET,   _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, KC_PGDN, KC_END,  KC_UP,   _______, _______, _______, \
+    _______, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______, \
+    _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+
   ),
+  [_NUMBER] = LAYOUT_ortho_4x12(
 
-  [_LOWER] = LAYOUT_ortho_4x12(
- KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
- KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
- _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
- RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+     RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+
   )
 
 };
